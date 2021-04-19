be58aa68b8d31e69735129a4d5cd16bfd000d791
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2021-04-19 23:53:05 +0200

    Added compensation for the temperature changes when calculating the GHe mass in the HP storage and the buffer tank.

--- startup/hevol-freia.cmd
+++ startup/hevol-freia.cmd
@@ -13,3 +13,12 @@ requireSnippet(hevol-freia-preSaveRestore.cmd)
 iocInit
 
 requireSnippet(hevol-freia-postSaveRestore.cmd)
+
+# Wait for connecting to Env-Container:TT001:sRdV and
+# set Cryo-HeVol:HPstorage:InOut = 0 in order to process
+# Cryo-HeVol:HPstorage:dMassHe without changing its value
+# and change the severity from INVALID to NO_ALARM
+
+epicsThreadSleep 3
+dbpf Cryo-HeVol:HPstorage:InOut 0
+
